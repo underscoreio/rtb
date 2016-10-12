0d0f7d10bf47b5d0685a7045fd280004e34686fb
Noel Welsh <noel@untyped.com>
2016-10-12 15:58:08 +0100

    Add a basic model for a bid request

--- server/src/main/scala/rtb/model.scala
+++ server/src/main/scala/rtb/model.scala
@@ -1,50 +1,22 @@
 package rtb
 
-import cats.data.ValidatedNel
-import cats.std.list._
-import cats.syntax.cartesian._
-import java.util.UUID
-
 object model {
-  import doodlebot.validation._
-  import doodlebot.syntax.validation._
-  import doodlebot.validation.Predicate._
+  final case class BidRequest(
+    bidId: String,
+    creativeSizing: Dimensions,
+    device: String,
+    priceFloor: Double,
+    origin: Origin,
+    country: String
+  )
 
-  // Messages to the client
-  final case class Authenticated(name: String, session: String)
-  final case class Log(offset: Int, messages: List[Message])
-  final case class Message(author: String, message: String)
-  final case class FormErrors(errors: InputError) extends Exception
+  sealed trait Origin
+  final case class App(bundle: String) extends Origin
+  final case class Web(url: String) extends Origin
 
-  // Messages from the client
-  final case class Login(name: Name, password: Password)
-
-  // Wrappers
-  final case class Name(get: String) extends AnyVal
-  object Name {
-    def validate(name: String): ValidatedNel[String,Name] = {
-      name.validate(lengthAtLeast(6) and onlyLettersOrDigits).map(n => Name(n))
-    }
-  }
-  final case class Email(get: String) extends AnyVal
-  object Email {
-    def validate(email: String): ValidatedNel[String,Email] = {
-      email.validate(containsAllChars("@.")).map(e => Email(e))
-    }
-  }
-  final case class Password(get: String) extends AnyVal
-  object Password {
-    def validate(password: String): ValidatedNel[String,Password] = {
-      password.validate(lengthAtLeast(8)).map(p => Password(p))
-    }
-  }
-  final case class Session(get: UUID = UUID.randomUUID()) extends AnyVal
-
-  // State
-  final case class User(name: Name, email: Email, password: Password)
-  object User {
-    def validate(name: String, email: String, password: String): ValidatedNel[String,User] = {
-      (Name.validate(name) |@| Email.validate(email) |@| Password.validate(password)).map { User.apply _ }
-    }
-  }
+  sealed trait Dimensions
+  case object FullScreen extends Dimensions
+  case object Mobile350x200 extends Dimensions
+  case object Mobile728x90 extends Dimensions
+  final case class NonStandard(width: Int, height: Int) extends Dimensions
 }
